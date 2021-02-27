5410df41598cfa7c37b1fc679b1281da390bb30f
Shang Shi <stoneshishang@gmail.com>
2021-02-27 18:19:38 -0500

    fixed the import linting warning and finished pandas crash course

--- 0-02-Pandas-Crash-Course/myPandasExample.py
+++ 0-02-Pandas-Crash-Course/myPandasExample.py
@@ -2,14 +2,21 @@ import numpy as np
 import pandas as pd
 
 # df = pd.read_csv('salaries.csv')
+# # print(df['Salary'].mean())
 
 # ser_of_bool = df['Age'] > 30
+# # print(df[ser_of_bool])
+# # print(df[df['Age'] > 30])
 
-# print(df['Age'].nunique())
+# # print(df['Age'].unique())
+# # print(df.columns)
+
+# # print(df.info())
 
 # print(df.index)
 
-mat = np.arange(0, 50).reshape(5, 10)
+mat = np.arange(0, 10).reshape(5, 2)
 # print(mat)
-df = pd.DataFrame(data=mat)
+df = pd.DataFrame(data=mat, columns=['A', 'B'], index=[
+                  'A', 'B', 'C', 'D', 'E'])
 print(df)
