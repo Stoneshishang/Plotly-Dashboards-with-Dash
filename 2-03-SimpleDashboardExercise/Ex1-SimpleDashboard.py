acf64394b6b5b7052bdce7014756b36ee0df8210
Shi <stoneshishang@gmail.com>
2021-03-29 16:38:54 -0400

    Create a simple dashboard with html.Div, go.Scatter() and go.Layout()

--- 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
+++ 2-03-SimpleDashboardExercise/Ex1-SimpleDashboard.py
@@ -8,38 +8,30 @@
 ######
 
 # Perform imports here:
-
-
-
-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # Launch the application:
-
+app = dash.Dash()
 
 # Create a DataFrame from the .csv file:
 
+df = pd.read_csv('../Data/OldFaithful.csv')
 
 # Create a Dash layout that contains a Graph component:
+app.layout = html.Div([dcc.Graph(id="old_faithful",
+                       figure={'data': [go.Scatter(x=df['X'], y=df['Y'], mode='markers')],
+                                 'layout':go.Layout(title='Old Faithful Eruptions',
+                                                    xaxis={
+                                                        'title': 'Duration'},
+                                                    yaxis={'title': 'Interval'})})])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    app.run_server()
 
 
 # Add the server clause:
